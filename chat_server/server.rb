eb708e9bd3c055bab3d9c6fcfef3e4c41a191a76
Zakary Bliss <zakarybliss@ucsb.edu>
2019-10-21 15:50:58 -0700

    Correctly retrieves username and password and generates JWT

--- chat_server/server.rb
+++ chat_server/server.rb
@@ -1,2 +1,33 @@
 require 'sinatra'
-require 'digest'
+require 'thin'
+require 'json'
+require 'jwt'
+
+post '/login' do 
+  username = params[:username]
+  password = params[:password]
+
+  if username ==  "" || password == "" 
+    [422, []]
+  else 
+    if 1==1 # TODO: Check database 
+      token = generate_JWT(password)
+      response = {}
+      response['token'] = token
+      [201, {}, response.to_json]
+    else 
+      [403, []]
+    end
+  end 
+
+  # puts username
+  # puts password
+end
+
+helpers do
+  def generate_JWT(secret_key)
+    payload = {}
+    token = JWT.encode payload, secret_key, 'HS256'
+    return token
+  end
+end 
