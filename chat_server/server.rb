f45712e52cc73103eb3670eb41b3a831b6d5a409
Zakary Bliss <zakarybliss@ucsb.edu>
2019-10-22 15:42:59 -0700

    Login now checks to see if username and password combo is valid and creates new users

--- chat_server/server.rb
+++ chat_server/server.rb
@@ -4,6 +4,10 @@ require 'thin'
 require 'json'
 require 'jwt'
 
+set :server, :thin
+connections = []
+users = {}
+
 post '/login' do 
   username = params[:username]
   password = params[:password]
@@ -11,32 +15,78 @@ post '/login' do
   if username ==  "" || password == "" 
     [422, []]
   else 
-    if 1==1 # TODO: Check database 
+    user = users[username]
+    if user == nil 
       token = generate_JWT(password)
-      response = {}
-      response['token'] = token
-      [201, {}, response.to_json]
-    else 
+        response = {}
+        response['token'] = token
+
+        users[username] = create_new_user(password, token)
+        puts users
+        [201, {}, response.to_json]
+    else   
+      if user['password'] == password
+        token = generate_JWT(password)
+        response = {}
+        response['token'] = token
+
+        users[username]['token'] = token
+        puts users
+        [201, {}, response.to_json]
+      else 
       [403, []]
-    end
+      end
+    end 
   end 
-
-  # puts username
-  # puts password
 end
-
-helpers do
-  def generate_JWT(secret_key)
-    payload = {}
-    token = JWT.encode payload, secret_key, 'HS256'
-    return token
-  end
-end 
 
 post '/:message' do 
 
 end
 
-get '/stream/:signed_token' do 
-
+get '/stream/:signed_token' do
+  decoded_token = decode_JWT(params[:signed_token])
+  if decoded_token == nil 
+    [403, []]
+  else 
+    stream(:keep_open) do |out|
+      connections << out 
+      out << "data: Welcome!\n\n"
+      out.callback {connections.delete(out)}
+      connections.reject!(&:closed?)
+    end 
+  end
 end  
+
+helpers do
+  def create_new_user(password, token) 
+      new_user = {}
+      new_user['password'] = password
+      new_user['token'] = token
+      new_user['stream'] = nil  
+      return new_user
+  end 
+
+  def generate_JWT(secret_key)
+    payload = {}
+    token = JWT.encode payload, secret_key, 'HS256'
+    return token
+  end
+
+  def decode_JWT(token)
+    if token != nil && token.start_with?('Bearer ')
+      begin
+        if token.split(' ')[1].split('.').length == 3
+          decoded_token = JWT.decode token.split(' ')[1], ENV['JWT_SECRET'], true, { algorithm: 'HS256' }
+          return decoded_token
+        else
+          return nil
+        end
+        rescue
+          return nil
+        end
+    else
+      return nil
+    end
+  end
+end 
