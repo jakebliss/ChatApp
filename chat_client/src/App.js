bb113da5add47b4ef2121ce241ead37b96d048f9
Jake Guida <jlguida@umail.ucsb.edu>
2019-10-23 13:51:33 -0700

    Messages component

--- chat_client/src/App.js
+++ chat_client/src/App.js
@@ -1,23 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
+import MessageList from './MessageList';
 import './App.css';
 
 function App() {
+  const messages = [
+    "jake says hello",
+    "jake says goodbye"
+  ]
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <MessageList messages = {messages}/>
       </header>
     </div>
   );
