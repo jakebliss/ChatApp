7a083f12846f66fdc4b0838579bfd1068c3c2019
Zakary Bliss <zakarybliss@ucsb.edu>
2019-10-22 20:59:46 -0700

    Created join SSE event

--- chat_server/server.rb
+++ chat_server/server.rb
@@ -7,8 +7,8 @@ require 'jwt'
 ENV['JWT_SECRET'] = "notasecret"
 
 set :server, :thin
-connections = []
-users = {}
+set :connections, {}
+set :users, {}
 
 post '/login' do 
   username = params[:username]
@@ -45,21 +45,27 @@ post '/:message' do
 end
 
 get '/stream/:signed_token' do
-  decoded_token = decode_JWT(params[:signed_token])
+  token = params[:signed_token]
+  decoded_token = decode_JWT(token)
   if decoded_token == nil 
     [403, []]
   else 
+    username = decoded_token[0]['data']
     stream(:keep_open) do |out|
-      connections << out 
+      connections[token] = out
       out << "data: Welcome!\n\n"
-      # disconnect(out)
+      join(username)
+      #disconnect(params[:signed_token])
       # out.callback {connections.delete(out)}
-      connections.reject!(&:closed?)
+      # connections.reject!(&:closed?)
     end 
   end
 end  
 
 helpers do
+  def connections; self.class.connections; end 
+  def users; self.class.users; end 
+
   def create_new_user(password, token) 
       new_user = {}
       new_user['password'] = password
@@ -88,14 +94,31 @@ helpers do
       end
   end
 
-  def disconnect(out)
-    data = {} 
+  def disconnect(token)
+    out = connections[token]
+
+    if stream != nil
+      data = {} 
+      data['created'] = Time.new(1993, 02, 24, 12, 0, 0, "+09:00").to_i
+
+      out << "data: " + data.to_json + "\n"
+      out << "event: " + "Disconnect\n"
+      out << "id: " + "tempID\n\n"
+
+      out.close
+      connections.delete(token)
+    end
+  end
+
+  def join(username) 
+    data = {}
+    data['user'] = username
     data['created'] = Time.new(1993, 02, 24, 12, 0, 0, "+09:00").to_i
-
-    out << "data: " + data.to_json + "\n"
-    out << "event: " + "Disconnect\n"
-    out << "id: " + "tempID\n\n"
-
-    out.close
+    
+    connections.each_value { |out| 
+      out << "data: " + data.to_json + "\n"
+      out << "event: " + "Join\n"
+      out << "id: " + "tempID\n\n"
+    }
   end
 end 
